6d53d36cf8e91859172f6900ac6765a1c57e2a42
Quema <105044971+Quema100@users.noreply.github.com>
2022-11-25 18:03:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 var consoles = prompt("이름을 입력하세요")                
 if(consoles != null){
 	if(consoles){         
-        alert(consoles,"님 안녕하세요.")
+		var links = prompt(consoles+"님 유튜브,깃허브,구글중에 검색할곳을 입력하세요")
+        if (links != null) {
+             if(links === "" ){
+
+                 alert("입력 받은 값이 없거나 이상합니다")
+        
+ 
+             }if (links==="유튜브"){
+                  var link1 = prompt(consoles+"님 유튜브에 검색할 내용을 입력하세요")
+		          window.open( "https://www.youtube.com/results?search_query="+link1)
+                    
+             }else if (links === "깃허브"){
+	              var link3 = prompt(consoles+"님 깃허브에 검색할 내용을 입력하세요")
+                   window.open( "https://github.com/search?q="+link3)
+             }else if (links === "구글"){
+	              var link2 = prompt(consoles+"님 구글에 검색할 내용을 입력하세요")
+                   window.open( "https://www.google.co.kr/search?q="+link2)
+             }else if(links){
+                  alert("입력 받은 값이 없거나 이상합니다")
+                           
+                 }
+       }else{
+          alert('취소되었습니다.')
+            }
+		
     }else if(consoles===""){
-        alert("무명님 안녕하세요.")
+		    var links = prompt("무명님 유튜브,깃허브,구글중에 검색할곳을 입력하세요")
+            if (links != null) {
+                 if(links === "" ){
+
+                     alert("입력 받은 값이 없거나 이상합니다")
+        
+ 
+                 }if (links==="유튜브"){
+                      var link1 = prompt("무명님 유튜브에 검색할 내용을 입력하세요")
+		               window.open( "https://www.youtube.com/results?search_query="+link1)
+                    
+                 }else if (links === "깃허브"){
+	                  var link3 = prompt("무명님 깃허브에 검색할 내용을 입력하세요")
+                       window.open( "https://github.com/search?q="+link3)
+                 }else if (links === "구글"){
+	                  var link2 = prompt("무명님 구글에 검색할 내용을 입력하세요")
+                       window.open( "https://www.google.co.kr/search?q="+link2)
+                 }else if(links){
+                      alert("입력 받은 값이 없거나 이상합니다")
+                           
+                     }
+           }else{
+              alert('취소되었습니다.')
+               }
+
     }
 
                      
 }else{
     alert("안녕히가세요.")
 	}
-
-var a=alert("Y=3,X=1에 가시면 깃허브로 바로가실수 있어요")
-var b=alert("Y=3,X=2에 가시면 구글로 바로가실수 있어요")
-var c=alert("Y=3,X=3에 가시면 유튜브로 바로가실수 있어요")
-var d=alert("Y=2,X=3에 가시면 뒤로 가실수 있어요")
-
-
-if (get_x()===1&&get_y()===3) {
-    window.location.href="https://github.com/"
-} else if (get_x()===2&&get_y()===3) {
-	window.location.href="https://www.google.co.kr/"
-} else if (get_x()===3&&get_y()===3) {
-	window.location.href="https://www.youtube.com/"
-} else if (get_x()===3&&get_y()===2) {
-	window.history.back()	
-} 
-

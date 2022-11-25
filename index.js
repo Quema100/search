7ed89ef5ac71f6351dc3b2f64dbf8265acd75917
Quema <105044971+Quema100@users.noreply.github.com>
2022-11-25 15:17:21 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var consoles = prompt("이름을 입력하세요")                
+if(consoles != null){
+	if(consoles){         
+        alert(consoles,"님 안녕하세요.")
+    }else if(consoles===""){
+        alert("무명님 안녕하세요.")
+    }
+
+                     
+}else{
+    alert("안녕히가세요.")
+	}
+
+var a=alert("Y=3,X=1에 가시면 깃허브로 바로가실수 있어요")
+var b=alert("Y=3,X=2에 가시면 구글로 바로가실수 있어요")
+var c=alert("Y=3,X=3에 가시면 유튜브로 바로가실수 있어요")
+var d=alert("Y=2,X=3에 가시면 유튜브로 바로가실수 있어요")
+var e=alert("Y=1,X=3에 가시면 유튜브로 바로가실수 있어요")
